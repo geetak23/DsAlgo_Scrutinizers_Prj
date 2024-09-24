6990517172af920e862b949941ecde51c14ba107
geetak23 <geetanjali.kuchana@gmail.com>
2024-09-24 16:09:08 -0500

    POM , stepdef, utilities updated
    POM , stepdef, utilities updated

--- src/test/java/POM/LandingPage.java
+++ src/test/java/POM/LandingPage.java
@@ -1,5 +1,32 @@
 package POM;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+import WebdriverManager.Webdrivermanager;
 
 public class LandingPage {
+	 private WebDriver driver;
 
+	    // Locator for the "Get Started" button
+	    private By getStartedButton = By.xpath("//button[text()='Get Started']");
+
+	    // Constructor to initialize the WebDriver
+	    public LandingPage(WebDriver driver) {
+	        this.driver = driver;
+	    }
+
+	    // Method to click on the "Get Started" button
+	    public void clickGetStarted() {
+	        driver.findElement(getStartedButton).click();
+	    }
+
+	    // Method to verify the current URL
+	    public boolean isAtDashboard(String expectedUrl) {
+	        return driver.getCurrentUrl().equals(expectedUrl);
+	    }
+
+	    // Method to navigate to the homepage
+	    public void goToHomePage() {
+	        driver.get("https://dsportalapp.herokuapp.com");
+	    }
 }
